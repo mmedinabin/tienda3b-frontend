8865ccd0c6de67296868eaf6ade5fdb2ecfe7cd4
Marco Medina <mmedina.informatica@gmail.com>
2026-02-26 00:08:16 -0400

    fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 import { useAuthStore } from '../store/auth.store'
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:5000/api',
-// })
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
-  //baseURL: 'http://localhost:5000/api',
-});
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+})
+
 
 /* =========================
    REQUEST INTERCEPTOR
